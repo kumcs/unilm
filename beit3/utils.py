0fcd9f1fa320299acdb7d6ed3ff99d088759dee8
Muhammad Adnan Kamal <kumcs2004@gmail.com>
2024-07-30 15:17:04 +0500

    dividie by zero error in logging.

--- beit3/utils.py
+++ beit3/utils.py
@@ -182,8 +182,8 @@ class MetricLogger(object):
             end = time.time()
         total_time = time.time() - start_time
         total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-        print('{} Total time: {} ({:.4f} s / it)'.format(
-            header, total_time_str, total_time / len(iterable)))
+        if len(iterable) != 0:
+            print('{} Total time: {} ({:.4f} s / it)'.format(header, total_time_str, total_time / len(iterable)))
 
 
 class TensorboardLogger(object):
